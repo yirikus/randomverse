a78b52540d46ac953caec6eca2a10777c4287951
terrmith <terrmith@90f0c836-b2b9-4b3f-a5f5-0a05b066677b>
2014-01-23 00:29:10 +0000

    BUILD shield, sonar property files

--- trunk/randomverse/src/cz/terrmith/randomverse/ability/Shield.java
+++ trunk/randomverse/src/cz/terrmith/randomverse/ability/Shield.java
@@ -1,8 +1,12 @@
 package cz.terrmith.randomverse.ability;
 
-import cz.terrmith.randomverse.core.sprite.SpriteCollection;
-import cz.terrmith.randomverse.core.sprite.SpriteLayer;
+import cz.terrmith.randomverse.core.image.ImageLoader;
+import cz.terrmith.randomverse.core.sprite.Sprite;
 import cz.terrmith.randomverse.core.sprite.properties.Ability;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Rectangle;
 
 /**
  * @author jiri.kus
@@ -10,7 +14,10 @@ import cz.terrmith.randomverse.core.sprite.properties.Ability;
 public class Shield extends Ability {
 
 
-	/**
+    private Sprite parent;
+    private static final int THICKNESS = 4;
+
+    /**
 	 * Creates an instance
 	 */
 	public Shield() {
@@ -21,16 +28,29 @@ public class Shield extends Ability {
 	 * Switches shield on or off
 	 */
 	@Override
-	public void useAbility() {
+	public void useAbility(Sprite parent) {
 		setActive(!isActive());
 		if (isActive()) {
 			//update parent [x, y]
-
+            this.parent = parent;
 		}
 	}
 
-	@Override
-	public void updateSprite() {
+    @Override
+    public void drawSprite(Graphics g, ImageLoader ims) {
+        Rectangle rect = parent.getBoundingBox();
+        g.setColor(new Color(0, 81, 250,50));
+        g.fillRect((int)rect.getX() - THICKNESS,
+                   (int)rect.getY() - THICKNESS,
+                   (int)rect.getWidth() + THICKNESS,
+                   (int)rect.getHeight() + THICKNESS);
+    }
+
+    public void updateSprite(Sprite parent) {
 		super.updateSprite();
+        if (isActive()) {
+            Rectangle rectangle = parent.getBoundingBox();
+            setPosition(rectangle.getX(), rectangle.getY());
+        }
 	}
 }
