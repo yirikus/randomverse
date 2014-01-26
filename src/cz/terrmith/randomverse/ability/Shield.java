88d3977dc50f1230dbdd7aae04c312e8ee278f39
terrmith <terrmith@90f0c836-b2b9-4b3f-a5f5-0a05b066677b>
2014-01-26 00:37:59 +0000

    BUILD TASKS shield recharge

--- src/cz/terrmith/randomverse/ability/Shield.java
+++ src/cz/terrmith/randomverse/ability/Shield.java
@@ -3,19 +3,23 @@ package cz.terrmith.randomverse.ability;
 import cz.terrmith.randomverse.core.image.ImageLoader;
 import cz.terrmith.randomverse.core.sprite.Sprite;
 import cz.terrmith.randomverse.core.sprite.properties.Ability;
+import cz.terrmith.randomverse.core.sprite.properties.Destructible;
 
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Rectangle;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author jiri.kus
  */
-public class Shield extends Ability {
+public class Shield extends Ability implements Destructible {
 
-
+    private static final int HEALTH_MAX = 10;
+    private int health = HEALTH_MAX;
     private Sprite parent;
-    private static final int THICKNESS = 4;
+    private static final int THICKNESS = 10;
+    private long lastUpdate = System.currentTimeMillis();
 
     /**
 	 * Creates an instance
@@ -41,18 +45,51 @@ public class Shield extends Ability {
     @Override
     public void drawSprite(Graphics g, ImageLoader ims) {
         Rectangle rect = parent.getBoundingBox();
-        g.setColor(new Color(0, 81, 250,50));
-        g.fillRect((int)rect.getX() - THICKNESS,
-                   (int)rect.getY() - THICKNESS,
+        g.setColor(new Color(0,250 - (health * 10), 250 - (health * 10),0 + (10 * health)));
+        g.fillRect((int)getXPosn() - THICKNESS,
+                   (int)getYPosn() - THICKNESS,
                    (int)rect.getWidth() + THICKNESS,
                    (int)rect.getHeight() + THICKNESS);
     }
 
-    public void updateSprite(Sprite parent) {
+    public void updateSprite() {
 		super.updateSprite();
-        if (isActive()) {
+        if (isActive() && parent != null) {
             Rectangle rectangle = parent.getBoundingBox();
             setPosition(rectangle.getX(), rectangle.getY());
         }
+        if (health < HEALTH_MAX && !isActive()) {
+            long now = System.currentTimeMillis();
+            if ((now - lastUpdate) >= 1500 ) {
+                lastUpdate = now;
+                health++;
+            } else {
+                System.out.println("shield replenish : " + (now - lastUpdate));
+            }
+        }
 	}
+
+    @Override
+    public int getTotalHealth() {
+        return HEALTH_MAX;
+    }
+
+    @Override
+    public int getCurrentHealth() {
+        return this.health;
+    }
+
+    @Override
+    public void reduceHealth(int amount) {
+        this.health = Math.max(0, health - amount);
+    }
+
+    @Override
+    public Rectangle getBoundingBox() {
+        Rectangle rect = parent.getBoundingBox();
+        return new Rectangle((int)rect.getX() - THICKNESS,
+                (int)rect.getY() - THICKNESS,
+                (int)rect.getWidth() + THICKNESS,
+                (int)rect.getHeight() + THICKNESS);
+    }
 }
